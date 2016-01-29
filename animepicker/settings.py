84eade6e911954b4d184e8fee7d0c897f3dc5967
Matthew Tinn <main.matthewtinn@gmail.com>
2016-01-29 11:34:42 +0000

    getting gitignore working

--- animepicker/settings.py
+++ animepicker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ukiyy9dz))%oq#amq#b)+03(r(^0kjf@*mg6s*l$q5!)o(tqs@'
+SECRET_KEY = 'Replace me in production' #secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Pick up local settings for development
+# If local settings does not exist, use settings
+try:
+    from animepicker.settings_local import *
+except ImportError as e:
+    pass
